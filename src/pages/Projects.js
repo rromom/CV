14969fd22db49221dc4b0ad616d0a8bd80313058
Ricardo Romo <52500188+rromom@users.noreply.github.com>
2021-03-05 03:54:36 +0000

    projects page finished

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,25 +2,28 @@ import React from 'react'
 import { Button, Card, Col, Container, Row } from 'react-bootstrap'
 import BasicLayaout from '../layaouts/BasicLayaout'
 import "./Projects.scss"
+import projects from "../utils/projects"
 
 export default function Projects() {
+    console.log(projects)
     return (
         <BasicLayaout MenuColor="#000">
             <Container className="projects">
                 <h1>Proyectos</h1>
                 <Row>
-                    <Col xs={12} sm={4} className="project">
+                    {projects.map((project,index) => (
+                        <Col  key={index} xs={12} sm={4} className="project">
                         <Card>
-                            <div className="image"/>
+                            <div className="image" style={{ backgroundImage: `url("${project.image}")` }}/>
                                 <Card.Body>
                                     <Card.Title>
-                                        Google
+                                        {project.title}
                                     </Card.Title>
                                     <Card.Text>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, quam. Eos qui magnam, eaque magni, ab voluptas deleniti nihil eum dignissimos amet excepturi ipsa voluptatem assumenda voluptates. Necessitatibus, illo iste.
+                                        {project.description}
                                     </Card.Text>
-                                    <a href="#" terget="_blank">
-                                        <Button primary>
+                                    <a href={project.url} terget="_blank">
+                                        <Button variant="primary">
                                             Ver Proyecto
                                         </Button>
                                     </a>
@@ -28,6 +31,8 @@ export default function Projects() {
                             
                         </Card>
                     </Col>
+                    ))}
+                    
                 </Row>
             </Container>
         </BasicLayaout>
---
+++ src/utils/projects.js
@@ -0,0 +1,38 @@
+import google from "../images/projects/google.png"
+import facebook from "../images/projects/facebook.png"
+import instagram from "../images/projects/instagram.png"
+import pinteres from "../images/projects/pinteres.png"
+import twitter from "../images/projects/twitter.png"
+
+export default [
+    {
+        title: "Google",
+        description: "El buscador y motor de busqueda google",
+        url: "https://google.ec/",
+        image: google 
+    },
+    {
+        title: "Twitter",
+        description: "Pagina de noticias y post",
+        url: "https://twitter.com/",
+        image: twitter
+    },
+    {
+        title: "Facebook",
+        description: "Red social",
+        url: "https://facebook.com/",
+        image: facebook
+    },
+    {
+        title: "Pinteres",
+        description: "Permite al usuario crear y administrar",
+        url: "https://www.pinterest.ca/",
+        image: pinteres
+    },
+    {
+        title: "Instagram",
+        description: "Red social con contenido multimedia",
+        url: "https://www.instagram.com/",
+        image: instagram
+    }
+]
